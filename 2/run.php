9e80a66ae8f087345243b78bb1c01f063bce7b79
Shane Mc Cormack <shanemcc@gmail.com>
2018-12-02 16:14:12 +0000

    Simplify part 1 a bit.

--- 2/run.php
+++ 2/run.php
@@ -3,33 +3,23 @@
 	require_once(dirname(__FILE__) . '/../common/common.php');
 	$boxes = getInputLines();
 
-	function hasMatchingCount($box, $count = 2) {
+	$hasThree = $hasTwo = [];
+	foreach ($boxes as $box) {
 		$letters = array_count_values(str_split($box));
 
-		foreach ($letters as $l => $v) {
-			if ($v == $count) { return true; }
+		foreach ($letters as $v) {
+			if ($v == 2) { $hasTwo[$box] = true; }
+			if ($v == 3) { $hasThree[$box] = true; }
 		}
-
-		return false;
 	}
-
-	$hasThree = 0;
-	$hasTwo = 0;
-	foreach ($boxes as $box) {
-		if (hasMatchingCount($box, 2)) { $hasTwo++; }
-		if (hasMatchingCount($box, 3)) { $hasThree++; }
-	}
-
-	$part1 = $hasThree * $hasTwo;
-
+	$part1 = count($hasThree) * count($hasTwo);
 	echo 'Part 1: ', $part1, "\n";
-
 
 	function getSame($box1, $box2, $differenceLimit = 1) {
 		$same = '';
 		$differenceCount = 0;
 		for ($i = 0; $i < strlen($box1); $i++) {
-			if ($box1{$i} === $box2{$i}) {
+			if ($box1{$i} == $box2{$i}) {
 				$same .= $box1{$i};
 			} else {
 				$differenceCount++;
@@ -44,7 +34,7 @@
 
 	for ($i = 0; $i < count($boxes); $i++) {
 		for ($j = $i; $j < count($boxes); $j++) {
-			$result = getSame($boxes[$i], $boxes[$j], 1);
+			$result = getSame($boxes[$i], $boxes[$j]);
 
 			if ($result !== FALSE) {
 				echo 'Part 2: ', $result, "\n";
