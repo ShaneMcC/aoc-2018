be6e39db2ddbc13cfcdec801c2bdfbc393a0c210
Shane Mc Cormack <shanemcc@gmail.com>
2018-12-09 07:35:07 +0000

    Allow building with php-jit docker image for testing.

--- docker.sh
+++ docker.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 IMAGE=shanemcc/aoc-2018-04
+DOCKERFILE="Dockerfile"
+if [ "${1}" = "--jit" ]; then
+	IMAGE="${IMAGE}-jit"
+	DOCKERFILE="${DOCKERFILE}-jit"
+	shift;
+fi;
 
 docker image inspect $IMAGE >/dev/null 2>&1
 if [ $? -ne 0 ]
 then
-    echo "One time setup: building docker image..."
+    echo "One time setup: building docker image ${IMAGE}..."
     cd docker
-    docker build . -t $IMAGE
+    docker build . -t $IMAGE --file ${DOCKERFILE}
     cd ..
 fi
 
