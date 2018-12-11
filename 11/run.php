def1e79a26e09346d8cc00ec11cf449ddbd3dcff
Shane Mc Cormack <shanemcc@gmail.com>
2018-12-11 06:04:20 +0000

    Tidy up. Stupid Slow.

--- 11/run.php
+++ 11/run.php
@@ -22,15 +22,35 @@
 	}
 
 	function getMax($size = 3) {
-		global $grid;
+		global $grid, $knownPowers;
 
 		$maxLevel = $maxX = $maxY = 0;
 
+		$lastLevel = NULL;
 		foreach (yieldXY(1, 1, 300 - $size, 300 - $size) as $x => $y) {
 			$level = 0;
-			foreach (yieldXY($x, $y, $x + $size, $y + $size, false) as $x2 => $y2) {
-				$level += $grid[$y2][$x2];
+
+			if (isset($knownPowers[$size - 1][$x][$y])) {
+				// Old level for 1-smaller grid.
+				$level += $knownPowers[$size - 1][$x][$y];
+
+				// Add the new Y Column
+				for ($x2 = $x; $x2 < $x + $size; $x2++) {
+					$level += $grid[$y + $size - 1][$x2];
+				}
+
+				// Add the new X Row excluding the corner.
+				for ($y2 = $y; $y2 < $y + $size - 1; $y2++) {
+					$level += $grid[$y2][$x + $size - 1];
+				}
+			} else {
+				// Calculate the whole thing.
+				foreach (yieldXY($x, $y, $x + $size, $y + $size, false) as $x2 => $y2) {
+					$level += $grid[$y2][$x2];
+				}
 			}
+
+			$knownPowers[$size][$x][$y] = $level;
 
 			if ($level > $maxLevel) {
 				$maxLevel = $level;
@@ -39,18 +59,17 @@
 			}
 		}
 
+		unset($knownPowers[$size - 1]);
+
 		return [$maxX, $maxY, $maxLevel];
 	}
 
-	[$maxX, $maxY, $maxLevel] = getMax(3);
-	echo $maxLevel, "\n";
-	echo $maxX, ', ', $maxY, "\n";
-
-	echo "\n\n";
+	list($maxX, $maxY, $maxLevel) = getMax(3);
+	echo 'Part 1: ', $maxX, ',', $maxY, ' (', $maxLevel, ')', "\n";
 
 	$maxLevel = $maxX = $maxY = $maxI = 0;
-
-	for ($i = 0; $i < 300; $i++) {
+	for ($i = 1; $i < 300; $i++) {
+		if (isDebug()) { echo $i, ' '; }
 		[$x, $y, $level] = getMax($i);
 
 		if ($level > $maxLevel) {
@@ -58,8 +77,7 @@
 			$maxX = $x;
 			$maxY = $y;
 			$maxI = $i;
-
-			echo $maxLevel, "\n";
-			echo $maxX, ',', $maxY, ',', $i, "\n";
 		}
 	}
+
+	echo 'Part 2: ', $maxX, ',', $maxY, ',', $maxI, ' (', $maxLevel, ')', "\n";
