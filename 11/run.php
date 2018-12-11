2a7d0d38e656fa14bd6c70385e3c4974d1b7b2fc
Shane Mc Cormack <shanemcc@gmail.com>
2018-12-11 05:16:44 +0000

    Messy Day 11, Part 2. Leaderboard 54.

--- 11/run.php
+++ 11/run.php
@@ -21,26 +21,45 @@
 		$grid[$y][$x] = getPowerLevel($x, $y, $gridSerial);
 	}
 
-	$maxLevel = $maxX = $maxY = 0;
-	foreach (yieldXY(1, 1, 300 - 3, 300 - 3) as $x => $y) {
-		$level = 0;
-		$level += $grid[$y][$x];
-		$level += $grid[$y][$x+1];
-		$level += $grid[$y][$x+2];
-		$level += $grid[$y+1][$x];
-		$level += $grid[$y+1][$x+1];
-		$level += $grid[$y+1][$x+2];
-		$level += $grid[$y+2][$x];
-		$level += $grid[$y+2][$x+1];
-		$level += $grid[$y+2][$x+2];
+	function getMax($size = 3) {
+		global $grid;
+
+		$maxLevel = $maxX = $maxY = 0;
+
+		foreach (yieldXY(1, 1, 300 - $size, 300 - $size) as $x => $y) {
+			$level = 0;
+			foreach (yieldXY($x, $y, $x + $size, $y + $size, false) as $x2 => $y2) {
+				$level += $grid[$y2][$x2];
+			}
+
+			if ($level > $maxLevel) {
+				$maxLevel = $level;
+				$maxX = $x;
+				$maxY = $y;
+			}
+		}
+
+		return [$maxX, $maxY, $maxLevel];
+	}
+
+	[$maxX, $maxY, $maxLevel] = getMax(3);
+	echo $maxLevel, "\n";
+	echo $maxX, ', ', $maxY, "\n";
+
+	echo "\n\n";
+
+	$maxLevel = $maxX = $maxY = $maxI = 0;
+
+	for ($i = 0; $i < 300; $i++) {
+		[$x, $y, $level] = getMax($i);
 
 		if ($level > $maxLevel) {
 			$maxLevel = $level;
 			$maxX = $x;
 			$maxY = $y;
+			$maxI = $i;
+
+			echo $maxLevel, "\n";
+			echo $maxX, ',', $maxY, ',', $i, "\n";
 		}
 	}
-
-
-	echo $maxLevel, "\n";
-	echo $maxX, ', ', $maxY, "\n";
