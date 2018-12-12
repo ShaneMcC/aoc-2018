019f510abc2cb6e1f9e9c484505bdb1bffd49a71
Shane Mc Cormack <shanemcc@gmail.com>
2018-12-12 05:44:58 +0000

    Day 12, Part 2.

--- 12/run.php
+++ 12/run.php
@@ -3,41 +3,72 @@
 	require_once(dirname(__FILE__) . '/../common/common.php');
 	$input = getInputLines();
 
-	$state = '';
+	$initial = '';
 	$changes = array();
 
 	foreach ($input as $details) {
 		if (preg_match('#initial state: (.*)#SADi', $details, $m)) {
-			$state = '...' . $m[1] . '............................';
-			$state = str_split($state);
+			$initial = '.....' . $m[1] . '.....';
+			$initial = str_split($initial);
 		} elseif (preg_match('#(.*) => (.*)#SADi', $details, $m)) {
 			$changes[$m[1]] = $m[2];
 		}
 	}
 
 
-	echo '0 ', implode($state), "\n";
-	for ($i = 1; $i <= 20; $i++) {
-		$newState = $state;
-		for ($p = 0; $p < count($state); $p++) {
-			$test = isset($state[$p - 2]) ? $state[$p - 2] : '.';
-			$test .= isset($state[$p - 1]) ? $state[$p - 1] : '.';
-			$test .= isset($state[$p]) ? $state[$p] : '.';
-			$test .= isset($state[$p + 1]) ? $state[$p + 1] : '.';
-			$test .= isset($state[$p + 2]) ? $state[$p + 2] : '.';
+	function doGenerations($count) {
+		global $initial, $changes;
 
-			$newState[$p] = isset($changes[$test]) ? $changes[$test] : '.';
+		$minus = 5;
+		$offset = 0;
+
+		$state = $initial;
+
+		$stable = 0;
+
+		if (isDebug()) { echo '0 ', implode($state), "\n"; }
+		for ($i = 1; $i <= $count; $i++) {
+			$newState = [];
+			for ($p = 0; $p < count($state); $p++) {
+				$test = isset($state[$p - 2]) ? $state[$p - 2] : '.';
+				$test .= isset($state[$p - 1]) ? $state[$p - 1] : '.';
+				$test .= isset($state[$p]) ? $state[$p] : '.';
+				$test .= isset($state[$p + 1]) ? $state[$p + 1] : '.';
+				$test .= isset($state[$p + 2]) ? $state[$p + 2] : '.';
+
+				$newState[$p] = isset($changes[$test]) ? $changes[$test] : '.';
+			}
+
+			$newState[] = '.';
+
+			$firstAlive = array_search('#', $newState);
+			if ($firstAlive > 5) {
+				$newState = array_slice($newState, $firstAlive - 5);
+				$offset += $firstAlive - 5;
+			}
+
+			if (implode('', $newState) == implode('', $state)) {
+				$stable++;
+			}
+			if ($stable > 2) {
+				if (isDebug()) { echo 'Stable at: ', $i, "\n"; }
+				$offset += $count - $i;
+				break;
+			}
+
+			$state = $newState;
+			if (isDebug()) { echo $i, ' ', implode('', $state), "\n"; }
 		}
 
-		$state = $newState;
-		echo $i, ' ', implode('', $state), "\n";
-	}
-
-	$res = 0;
-	for ($c = 0; $c < count($state); $c++) {
-		if ($state[$c] == '#') {
-			$res += $c - 3;
+		$res = 0;
+		for ($c = 0; $c < count($state); $c++) {
+			if ($state[$c] == '#') {
+				$res += $c - $minus + $offset;
+			}
 		}
+
+		return $res;
 	}
 
-	echo 'Part 1: ', $res, "\n";
+	echo 'Part 1: ', (isDebug() ? "\n" : ''), doGenerations(20), "\n";
+	echo 'Part 2: ', (isDebug() ? "\n" : ''), doGenerations(50000000000), "\n";
