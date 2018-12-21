7ca2ff263fa04805131979446a1a1e90f113463a
Shane Mc Cormack <shanemcc@gmail.com>
2018-12-21 13:19:45 +0000

    Naive part 2, runs for ages.

--- 21/run.php
+++ 21/run.php
@@ -10,8 +10,10 @@
 	$vm->setDebug(isDebug());
 	$vm->setLimit(-1);
 
-	$part1 = 0;
-	$vm->addReadAhead(function ($vm) use (&$part1) {
+	$part1 = $part2 = 0;
+	$seen = [];
+
+	$vm->addReadAhead(function ($vm) use (&$part1, &$part2, &$seen) {
 		$loc = $vm->getLocation();
 		$readAheadOps = 0;
 
@@ -22,17 +24,22 @@
 		// Check when we compare reg 0 to something.
 		if ($data[0][0] == 'eqrr' && ($data[0][1][0] == 0 || $data[0][1][1] == 0)) {
 			$wanted = $data[0][1][0] == 0 ? $data[0][1][1] : $data[0][1][0];
+			$val = $vm->getReg($wanted);
 
-			$part1 = $vm->getReg($wanted);
+			if ($part1 == 0) { $part1 = $val; }
 
-			$vm->end(0);
-			$vm->setReg($vm->ip, $vm->getLocation()); // Horrible IP Counter.
-			return $vm->getLocation();
+			if (in_array($val, $seen)) {
+				$part2 = $val;
+
+				$vm->end(0);
+				$vm->setReg($vm->ip, $vm->getLocation()); // Horrible IP Counter.
+				return $vm->getLocation();
+			}
+			$seen[] = $val;
 		}
 
 		return FALSE;
 	});
-
 
 
 	$vm->loadProgram(Day19VM::parseInstrLines($input));
@@ -40,3 +47,4 @@
 	$vm->run();
 
 	echo 'Part 1: ', $part1, "\n";
+	echo 'Part 2: ', $part2, "\n";
