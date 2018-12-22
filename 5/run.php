0f0cd080378411961bba9b275f149073c2aacb7c
Shane Mc Cormack <shanemcc@gmail.com>
2018-12-22 23:25:35 +0000

    Day 5: Speed up.

--- 5/run.php
+++ 5/run.php
@@ -1,36 +1,31 @@
 #!/usr/bin/php
 <?php
 	require_once(dirname(__FILE__) . '/../common/common.php');
-	$input = getInputLine();
+	$input = str_split(strrev(getInputLine()));
 
-	// Build all the removal pairs required in this input.
-	$removals = [];
-	for ($unit = 65; $unit < 91; $unit++) {
-		$removals[] = chr($unit) . chr($unit ^ 32);
-		$removals[] = chr($unit ^ 32) . chr($unit);
-	}
+	function react($input, $skip = NULL) {
+		$out = [];
 
-	function react($input) {
-		global $removals;
-		do {
-			$input = str_replace($removals, '', $input, $count);
-		} while ($count > 0);
+		foreach ($input as $in) {
+			if ($skip != null && ($in == $skip || (ord($skip) ^ ord($in)) == 32)) {
+				continue;
+			} else if (!empty($out) && (ord(end($out)) ^ ord($in)) == 32) {
+				array_pop($out);
+			} else {
+				$out[] = $in;
+			}
+		}
 
-		return $input;
+		return $out;
 	}
 
 	$part1 = react($input);
-	echo 'Part 1: ', strlen($part1), "\n";
+	echo 'Part 1: ', count($part1), "\n";
 
-	// Start smaller.
-	$input = $part1;
-	$shortest = strlen($input);
+	$shortest = count($part1);
 	for ($unit = 65; $unit < 91; $unit++) {
-		$newInput = str_replace([chr($unit), chr($unit ^ 32)], '', $input, $count);
-		if ($count > 0) {
-			$result = react($newInput);
-			if (strlen($result) < $shortest) { $shortest = strlen($result); }
-		}
+		$result = react($part1, chr($unit));
+		if (count($result) < $shortest) { $shortest = count($result); }
 	}
 
 	echo 'Part 2: ', $shortest, "\n";
--- common/elfcode.php
+++ common/elfcode.php
@@ -76,8 +76,6 @@
 
 
 
-
-
 	// Output.
 	$colWidth = 40;
 
