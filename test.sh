33928103450a33d365bba9faed6fcb93e841f923
Shane Mc Cormack <shanemcc@gmail.com>
2019-12-10 01:24:14 +0000

    Update test.sh to handle day 25.

--- test.sh
+++ test.sh
@@ -5,20 +5,24 @@ cd "$(dirname "$0")"
 for DAY in `seq 1 25`; do
 	if [ -e ${DAY} ]; then
 		echo -n "Day ${DAY}:"
-		if [ -e ${DAY}/answers.txt ]; then
+		if [ -e ${DAY}/answers.txt -a $(cat ${DAY}/answers.txt | wc -l) -ne 0 ]; then
 			PART1=$(cat ${DAY}/answers.txt | head -n 1)
-			PART2=$(cat ${DAY}/answers.txt | head -n 2 | tail -n 1)
+			if [ $(cat ${DAY}/answers.txt | wc -l) -eq 1 ]; then
+				PART2=""
+			else
+				PART2=$(cat ${DAY}/answers.txt | head -n 2 | tail -n 1)
+			fi;
 
 			RESULT=$(${DAY}/run.php 2>/dev/null | grep -Pzl ".*${PART1}.*\n.*${PART2}.*")
 			# RESULT=$(./docker.sh ${DAY} 2>/dev/null | grep -Pzl ".*${PART1}.*\n.*${PART2}.*")
 
 			if [ "${RESULT}" = "" ]; then
-				echo -e "\033[0;32m" "Fail." "\033[0m";
+				echo -e "\033[1;31m" "Fail." "\033[0m";
 			else
 				echo -e "\033[0;32m" "Success." "\033[0m";
 			fi;
 		else
-			echo -e "\033[0;32m" "Untested." "\033[0m";
+			echo -e "\033[1;31m" "Untested." "\033[0m";
 		fi;
 	fi;
 done;
